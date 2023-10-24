9988ec8ee6e156cd1e3dd1718d68e44a5c2a1bce
Felipe Greg <mrfelipesensei@gmail.com>
2023-10-24 15:20:51 -0300

    Padronizando cabeçalho

--- 16/crud_1/index.php
+++ 16/crud_1/index.php
@@ -5,16 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FITNOW - Users</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet">
+    <link href="assests/css/boot.css" rel="stylesheet"> <!--boot.css-->
+    <link href="assests/css/fonticon.css" rel="stylesheet"> <!--fonticon.css-->
+    <link rel="stylesheet" href="assests/css/modal.css"> <!--modal.css-->
+    <link rel="stylesheet" href="assests/css/login.css"> <!--login.css-->
+    <link rel="stylesheet" href="assests/css/styles.css">
 </head>
 <body>
-    <header>
-        <h1>FIT NOW</h1>
-        <p>Tabela de Usuários Cadastrados</p>
+    <header class="main_header">
+        <div class="main_header_content">
+            <a href="home.html" class="logo">
+                <img id="logofit" src="img/logofit.png" alt="FITNOW - A qualquer hora e qualquer lugar"
+                    title="FITNOW - A qualquer hora e qualquer lugar"></a>
+
+            <nav class="main_header_content_menu">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="perfis/index.php">Perfil</a></li>
+                    <li><a href="#">Usuários</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
-    <nav>
-        <a href="home.html">Home</a>
-        <a href="perfis/index.php">Perfil</a>
-        <a href="#">Usuários</a>
-    </nav>
 </body>
 </html>
--- 16/crud_1/perfis/index.php
+++ 16/crud_1/perfis/index.php
@@ -20,14 +20,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet">
+    <link href="../assests/css/boot.css" rel="stylesheet"> <!--boot.css-->
+    <link href="../assests/css/fonticon.css" rel="stylesheet"> <!--fonticon.css-->
+    <link rel="stylesheet" href="../assests/css/modal.css"> <!--modal.css-->
+    <link rel="stylesheet" href="../assests/css/login.css"> <!--login.css-->
+    <link rel="stylesheet" href="../assests/css/styles.css">
     <title>Perfil</title>
 </head>
 <body>
-    <nav>
-        <a href="#">Home</a>
-        <a href="index.php">Perfil</a>
-        <a href="#">Usuários</a>
-    </nav>
+    <header class="main_header">
+        <div class="main_header_content">
+            <a href="home.html" class="logo">
+                <img id="logofit" src="../img/logofit.png" alt="FITNOW - A qualquer hora e qualquer lugar"
+                    title="FITNOW - A qualquer hora e qualquer lugar"></a>
+
+            <nav class="main_header_content_menu">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="../perfis/index.php">Perfil</a></li>
+                    <li><a href="#">Usuários</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
     <main>
         <h1>Perfil</h1>
         <section class="section__btn">
