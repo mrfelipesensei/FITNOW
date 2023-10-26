7f4c8f04c2c1493400c9fec03b1cc2a6fefa5ae3
Felipe Greg <mrfelipesensei@gmail.com>
2023-10-26 15:34:47 -0300

    CRUD2/config.php

---
+++ CRUD2/config.php
@@ -0,0 +1,14 @@
+<?php
+    define('HOST','localhost');
+    define('USER','root');
+    define('PASS','');
+    define('BASE','fitnowbd');
+
+    $conexao = new mysqli(HOST,USER,PASS,BASE);
+
+
+
+
+
+
+?>
