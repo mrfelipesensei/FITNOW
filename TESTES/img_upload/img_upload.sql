0fdcfcbebd986f92d10aa9ff7d8035eb6d61d3eb
Felipe Greg <mrfelipesensei@gmail.com>
2023-10-31 17:49:35 -0300

    excluindo testes de upload

--- TESTES/img_upload/img_upload.sql
+++
@@ -1,9 +0,0 @@
-create database img_upload;
-use img_upload;
-
-CREATE TABLE images (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    image_name VARCHAR(255),
-    image_data LONGBLOB,
-    image_description TEXT
-);
