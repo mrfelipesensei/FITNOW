3c351842b82644f6d5fcd825546e78e7e677a422
Felipe Greg <mrfelipesensei@gmail.com>
2023-11-06 15:27:59 -0300

    estilo admin_painel.php finalizado

--- CRUD2/login/admin_painel.php
+++ CRUD2/login/admin_painel.php
@@ -6,20 +6,60 @@ include("protect.php");
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Painel</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
+    <link href="../assests/css/boot.css" rel="stylesheet"> <!--boot.css-->
+    <link href="../assests/css/style.css" rel="stylesheet"> <!--style.css-->
+    <link rel="stylesheet" href="assests/css/index_style.css">
+    <link rel="shortcut icon" href="img/icons8-marcador-50.png">
+    <link rel="stylesheet" href="../assests/css/login_style.css">
+    <link rel="stylesheet" href="../assests/css/admin.css">
+    <script src="index.js" defer></script>
+    <title>ADMIN</title>
 </head>
+<!--Cabeçalho-->
+<header class="main_header">
+    <div class="main_header_content">
+        <a href="../index.html" class="logo">
+            <img id="fitlogo_header" src="../img/logofit.png" alt="FITNOW - A qualquer hora e qualquer lugar"
+                title="FITNOW - A qualquer hora e qualquer lugar"></a>
+
+        <nav class="main_header_content_menu">
+            <ul>
+                <li><a href="logout.php">Sair</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<!--Fim Cabeçalho-->
 <body>
-    Bem vindo ao Painel, <?php echo $_SESSION["nome"]; ?>.
-    <p>
-        <a href="../usuarios/index.php">Pesquisar/Alterar Usuários</a>
-    </p>
-    <p>
-        <a href="../academias/index.php">Pesquisar/Alterar Academias</a>
-    </p>
-    <p>
+    <div>
+        <p id="bemvindo">
+            Bem vindo ao Painel, <?php echo $_SESSION["nome"]; ?>.
+        </p>
+    </div>
+    <div class="box">
+        <div class="item">
+            <p>
+                <a href="../usuarios/index.php">
+                    <img src="../img/profile.png" alt="">
+                    <p class="text" >Pesquisar/Alterar Usuários</p>
+                </a>
+            </p>
+        </div>
+        <div class="item">
+            <p>
+                <a href="../academias/index.php">
+                    <img src="../img/gym.png" alt="">
+                    <p class="text" >Pesquisar/Alterar Academias</p>
+                </a>
+            </p>
+        </div>
+    </div>
+    <!-- <p>
         <a href="logout.php">Sair</a>
-    </p>
+    </p> -->
 </body>
 </html>
