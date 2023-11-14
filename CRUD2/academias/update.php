8914ccb8ef0d98f1e0e4ee11019b30eda7ddf01a
Felipe Greg <mrfelipesensei@gmail.com>
2023-11-14 14:42:02 -0300

    corrigindo erro de alterar academias

--- CRUD2/academias/update.php
+++ CRUD2/academias/update.php
@@ -9,14 +9,13 @@ $id = filter_input(INPUT_POST,'id', FILTER_SANITIZE_NUMBER_INT)?? 0;
 $nome = filter_input(INPUT_POST,'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 $cnpj = filter_input(INPUT_POST,'cnpj', FILTER_SANITIZE_SPECIAL_CHARS);
 $horarios = filter_input(INPUT_POST,'horarios', FILTER_SANITIZE_SPECIAL_CHARS);
-$bairro = filter_input(INPUT_POST,'bairro', FILTER_SANITIZE_SPECIAL_CHARS);
 $modalidades = filter_input(INPUT_POST,'modalidades', FILTER_SANITIZE_SPECIAL_CHARS);
 $valores = filter_input(INPUT_POST,'valores', FILTER_SANITIZE_SPECIAL_CHARS);
 
 // var_dump($id, $nome, $cnpj, $horarios, $bairro, $modalidades, $valores);
 
 $dao = new AcademiaDAO();
-$result = $dao->update($id, $nome, $cnpj, $horarios, $bairro, $modalidades, $valores);
+$result = $dao->update($id, $nome, $cnpj, $horarios, $modalidades, $valores);
 
 if ($result) {
     header('location: index.php?msg=Academia atualizada com sucesso!');
